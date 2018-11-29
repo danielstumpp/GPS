19b3bf9e67e320709c78f0e65b43072f3dbb9179
Rachel Misbin <rachelmisbin@pitt.edu>
2018-11-29 17:37:52 -0500

    Uploaded unfixed test data and haversine.py

--- test_processing.py
+++ test_processing.py
@@ -1,3 +1,6 @@
+import math
+import Haversine
+
 def main():
 	tx_lat = 0
 	tx_long = 0
@@ -6,11 +9,12 @@ def main():
 	dist = 0
 
 	filename = raw_input("Enter the filename: ")
+	fix = raw_input("Care about fix (y/n)? ")
 
 	# Read GPS data from user about receiver location
-	rx_lat  = input("Enter receiver latitude: ")
-	rx_long = input("Enter receiver longitude: ")
-	rx_alt  = input("Enter receiver altitude: ")
+	rx_lat  = raw_input("Enter receiver latitude: ")
+	rx_long = raw_input("Enter receiver longitude: ")
+	rx_alt  = raw_input("Enter receiver altitude: ")
 
 	file = open(filename, "r")
 	for line in file:
@@ -18,13 +22,15 @@ def main():
 			data = line.split(',')
 
 			# Exit if GPS didn't get a fix
-			if data[5] != 1:
+			if (data[5] != 1) & (fix == "y"):
 				print "Error: no fix"
 				quit()
 			
 			# Read GPS data about transceiver location
-			tx_lat  = data[1] + data[2]
-			tx_long = data[3] + data[4]
+			# tx_lat  = data[1] + data[2]
+			# tx_long = data[3] + data[4]
+			tx_lat = data[1]
+			tx_long = data[3]
 			tx_alt  = data[8]
 
 			dist = calcDist(rx_lat, rx_long, rx_alt, tx_lat, tx_long, tx_alt)
@@ -40,8 +46,6 @@ def calcDist(rx_lat, rx_long, rx_alt, tx_lat, tx_long, tx_alt):
 	# TODO: perform distance calculation
 	
 
-	
 	return -1
-
 if __name__ == "__main__":
 	main()
